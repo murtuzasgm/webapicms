8bd66ad32f2d4f45dae2b84aa096b9f6f5fd61c3
syed <sgmrazvi1@gmail.com>
2018-12-04 15:37:07 +0530

    syed

--- WebApplicationcms/WebApplicationcms/Controllers/ValuesController.cs
+++ WebApplicationcms/WebApplicationcms/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplicationcms.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value11", "value22" };
+            return new string[] { "value11", "value22syed" };
         }
 
         // GET api/values/5
