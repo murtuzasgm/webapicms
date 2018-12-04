b777c4776f57ee7fa14cee74a4be70f94c497fe5
syed <sgmrazvi1@gmail.com>
2018-12-04 15:50:27 +0530

    Update ValuesController.cs
    gulam added

--- WebApplicationcms/WebApplicationcms/Controllers/ValuesController.cs
+++ WebApplicationcms/WebApplicationcms/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplicationcms.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value11", "value2" };
+            return new string[] { "value11", "value2222gulam" };
         }
 
         // GET api/values/5
