8fd226fca79ab988d8e365ecc3095e4ef595ce74
syed <sgmrazvi1@gmail.com>
2018-12-04 15:52:12 +0530

    Update ValuesController.cs
    raz added

--- WebApplicationcms/WebApplicationcms/Controllers/ValuesController.cs
+++ WebApplicationcms/WebApplicationcms/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplicationcms.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value11", "value22syed" };
+            return new string[] { "value11", "value22syedraz" };
         }
 
         // GET api/values/5
